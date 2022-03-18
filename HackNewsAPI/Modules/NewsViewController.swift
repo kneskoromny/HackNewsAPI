bc40defee3975782c8e725179f922cc2f31bc16f
kneskoromny <kneskoromny@gmail.com>
2022-03-18 12:04:07 +0300

    moya + layoutKit

--- HackNewsAPI/Modules/NewsViewController.swift
+++ HackNewsAPI/Modules/NewsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LayoutKit
 
 protocol NewsViewProtocol: AnyObject {
     
--- HackNewsAPI/Services/NetworkService.swift
+++ HackNewsAPI/Services/NetworkService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Moya
 
 // MARK: - Protocol requirements
 protocol NetworkServiceProtocol {
