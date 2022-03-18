9926966b5f346f2287e6efa5daa179328178f83e
kneskoromny <kneskoromny@gmail.com>
2022-03-18 09:46:19 +0300

    mvp prepare + constants init + delete storyboard

---
+++ HackNewsAPI/Modules/NewsPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  NewsPresenter.swift
+//  HackNewsAPI
+//
+//  Created by Кирилл Нескоромный on 18.03.2022.
+//
+
+import Foundation
---
+++ HackNewsAPI/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  HackNewsAPI
+//
+//  Created by Кирилл Нескоромный on 18.03.2022.
+//
+
+import UIKit
+
+enum K {
+    static let appName = "Hacker News"
+    
+    enum Colors {
+        static let white = UIColor.systemBackground
+        static let light = UIColor.systemGray4
+        static let dark = UIColor.label
+    }
+}
